ba6520cf32464ade08e674cf47f1718b0edd0f54
vadgay <40436284+vadgay@users.noreply.github.com>
2018-06-20 19:24:21 +0300

    example.php

--- example.php
+++ example.php
@@ -1,62 +1,191 @@
-<?php
 
-require_once './LiteDonateSDK/LiteDonate.php';
 
-use LiteDonateSDK\LiteDonate;
-
-// ID демо-магазина: http://demo.autodonate.su
-$sdk = new LiteDonate(13);
-
-if(isset($_POST['buy'])) {
-	$url = $sdk->createPay();
-	if($url)
-		header("Location: " . $url);
-}
-
-$shopInfo = $sdk->getInfo();
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title><?=$shopInfo['name']?></title>
+    <meta charset="utf-8">
+    <title>FoxMC - Покупка донат-услуг</title>
+	<link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./assets/bootstrap.css">
+    <link rel="stylesheet" href="./assets/site.css">
+    <link rel="stylesheet" href="./assets/styles.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
+    <script src="https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"></script>
+    <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./assets/jquery.countdown.min.js"></script>
 </head>
+
 <body>
-	<h1 align="center">
-		<?=$shopInfo['name']?>
-	</h1>
+    <nav class="navbar navbar-inverse bg-inverse navbar-toggleable-md">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="/"><span class="logo">FoxMC</span><strong>  Survival #1</strong></a>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item"><a href="/" class="nav-link"><i class="fa fa-home"></i> Главная</a></li>
+				  <li class="nav-item"><a href="rules.php" class="nav-link"><i class="fa fa-pencil"></i> Правила</a></li>
+				  <li class="nav-item"><a href="buy.php" class="nav-link"><i class="fa fa-money"></i> Как купить донат?</a></li>
+				  <li class="nav-item"><a href="donate.php" class="nav-link"><i class="fa fa-database"></i> Описание привилегий</a></li>
+				  <li class="nav-item"><a href="console.php" class="nav-link"><i class="fa fa-server"></i> Консоль</a></li>
+				  <li class="nav-item"><a href="https://vk.me/fox_m_c" class="nav-link"><i class="fa fa-send"></i> Контакты</a></li>
+</ul>
+<span class="navbar-text">
+                <span class="wobble badge badge-danger">Онлайн: 2491/5000</span>
+                   <span class="badge badge-primary" onclick="prompt('Скопируйте наш IP адрес и вставьте в ваш клиент:','FoxMc.Ru'); return false;">IP: FoxMC.ru</span>
+                </span>
+            </div>
+        </div>
+    </nav>
+    <div class="container" style="margin-top: 6em;">
+        <div class="row">
+	<div class="col-12">
+		
+	</div>
+</div>
+        <center>
+    <img src="assets/banner-min.png" tppabs="FoxMC.Ru/styles/img/banner-min.png" class="img-fluid" />
+         </center>
+         <br>
+         <div class="card">
+            <div class="card-body">
+               <div class="row">
+                  <div class="col-md-6">
+                     <ul class="nav nav-tabs nav-fill" style="margin-bottom: 15px">
+                                                <li class="nav-item">
+                           <a class="nav-link active" href="/">Survival #1</a>
+                        </li>
+                                                <li class="nav-item">
+                           <a class="nav-link " href="survival-2.php">Survival #2</a>
+                        </li>
+                     
+                    </ul>
+                    <br>
 	<div class="products">
 		<form action="" method="post" align="center">
-			<input type="text" name="nickname" placeholder="Никнейм" required> <br>
-			<input type="text" name="coupon" placeholder="Купон, если есть"> <br>
-			<select name="productId" required>
-				<option value="0" disabled selected>Выберите товар</option>
-				<?php foreach($sdk->getProducts() as $product): ?>
-					<option value="<?=$product['id']?>">
-						<?=$product['name']?> (<?=$product['price']?>руб.)
+		    <label for="username" class="control-label"><i class="fa fa-user" aria-hidden="true"></i> Введите ваш ник:</label>
+			<input type="text" class="input-nick form-control" name="nickname" placeholder="Заполните данное поле" required> <br>
+			<label for="selid" class="control-label"><i class="fa fa-diamond" aria-hidden="true"></i> Выберите товар:</label>
+            <select id="productId" name="productId" class="input-nick form-control">
+							           	<option val='1' disabled selected="">Выберите товар</option>
+									<option value="4894">
+						Вип (1руб.)
 					</option>
-				<?php endforeach; ?>
-			</select> <br>
-			<input type="submit" name="buy" value="Купить">
+									<option value="4896">
+						Премиум (10руб.)
+					</option>
+									<option value="4897">
+						Анти-Грифер (75руб.)
+					</option>
+									<option value="4898">
+						Модер (20руб.)
+					</option>
+									<option value="4899">
+						Админ (25руб.)
+					</option>
+									<option value="4900">
+						Элита (30руб.)
+					</option>
+									<option value="4901">
+						Гл.Админ (35руб.)
+					</option>
+									<option value="4902">
+						Король (40руб.)
+					</option>
+									<option value="4903">
+						Босс (45руб.)
+					</option>
+									<option value="4904">
+						Создатель (50руб.)
+					</option>
+									<option value="4905">
+						Титан (55руб.)
+					</option>
+									<option value="4906">
+						ОП + Доступ к приватам (25руб.)
+					</option>
+									<option value="4907">
+						Хелпер (30руб.)
+					</option>
+									<option value="4908">
+						Owner (40руб.)
+					</option>
+									<option value="4909">
+						Все права (50руб.)
+					</option>
+									<option value="4910">
+						Fox (65руб.)
+					</option>
+									<option value="4911">
+						Анти-Грифер (75руб.)
+					</option>
+									<option value="4912">
+						Купил Сервер (80руб.)
+					</option>
+									<option value="4913">
+						Троль (90руб.)
+					</option>
+									<option value="4914">
+						Консоль ☾Осталось 4 ШТ☽ (59руб.)
+					</option>
+									<option value="4915">
+						Владелец + /op + КОНСОЛЬ + * (123руб.)
+					</option>
+									<option value="4916">
+						Эпик + СБОРКА СЕРВЕРА (278руб.)
+					</option>
+									<option value="4917">
+						☾ＭＯＮＳＴＥＲ☽ (368руб.)
+					</option>
+									<option value="4918">
+						RCON + FTP + SSH + КОНСОЛЬ (899руб.)
+					</option>
+									<option value="4919">
+						Администратор Сервера (2500руб.)
+					</option>
+									<option value="4920">
+						Кейсы 1 ШТ (3руб.)
+					</option>
+									<option value="4921">
+						Кейсы 5 ШТ (10руб.)
+					</option>
+									<option value="4922">
+						Кейсы 10 ШТ (15руб.)
+					</option>
+									<option value="4923">
+						Кейсы 50 ШТ (30руб.)
+					</option>
+									<option value="4924">
+						Кейсы 100 ШТ (50руб.)
+					</option>
+									<option value="4925">
+						Кейсы 1000 ШТ (100руб.)
+					</option>
+									<option value="4926">
+						Разбан (35руб.)
+					</option>
+							</select> <br>
+			<input type="submit" name="buy" class="submit buy_btn btn btn-warning btn-block"" value="Купить / Доплатить">
 		</form>
-	</div>
-	<br>
-	<div class="desc">
-		<h2 align="center">Описание товаров</h1>
-		<?php foreach($sdk->getProducts() as $product): ?>
-			<h4><?=$product['name']?></h4>
-			<p><?=$product['description']?></p>
-			<hr>
-		<?php endforeach; ?>
-	</div>
-	<br>
-	<div class="last">
-		<h2 align="center">Последние покупки</h2>
-		<?php foreach($sdk->getLastPurchases() as $purchase): ?>
-			<p>Игрок <b><?=$purchase['nickname']?></b></p>
-			<p>купил <?=is_array($purchase['product']) ? $purchase['product']['name'] : $purchase['product']?></p>
-			<p>за <?=($purchase['price'] + $purchase['commission'])?> руб.</p>
-			<hr>
-		<?php endforeach; ?>
-	</div>
-</body>
+                     
+                    </div>
+                </div>
+<div class="col-md-6">
+<a><img src="assets/console.jpg" class="img-fluid" style="margin-bottom: 15px"></a>
+<br>
+<img src="assets/misha_huyesos.png" class="img-fluid">
 </html>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<br>
+<footer>
+<center align=center class="contacts"><a href="https://vk.com/towaxakin"><i class=""></i> Разработано XakinYT</a></li>
