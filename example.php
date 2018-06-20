6aa74e455fcd2ca15cc810752e76382c5cd394e5
vadgay <40436284+vadgay@users.noreply.github.com>
2018-06-20 21:54:25 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,62 +1,68 @@
 <?php
-
-require_once './LiteDonateSDK/LiteDonate.php';
-
-use LiteDonateSDK\LiteDonate;
-
-// ID демо-магазина: http://demo.autodonate.su
-$sdk = new LiteDonate(13);
-
-if(isset($_POST['buy'])) {
-	$url = $sdk->createPay();
-	if($url)
-		header("Location: " . $url);
-}
-
-$shopInfo = $sdk->getInfo();
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title><?=$shopInfo['name']?></title>
-</head>
-<body>
-	<h1 align="center">
-		<?=$shopInfo['name']?>
-	</h1>
-	<div class="products">
-		<form action="" method="post" align="center">
-			<input type="text" name="nickname" placeholder="Никнейм" required> <br>
-			<input type="text" name="coupon" placeholder="Купон, если есть"> <br>
-			<select name="productId" required>
-				<option value="0" disabled selected>Выберите товар</option>
-				<?php foreach($sdk->getProducts() as $product): ?>
-					<option value="<?=$product['id']?>">
-						<?=$product['name']?> (<?=$product['price']?>руб.)
-					</option>
+define('INCLUDE_CHECK',true);
+header('Content-Type: text/html;charset=UTF-8');
+include "./incl/init.php";
+?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="<?=$config['domain']?>/css/style.css">
+		<link rel="stylesheet" type="text/css" href="<?=$config['domain']?>/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="<?=$config['domain']?>/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?=$config['domain']?>/css/bootstrap-responsive.css">
+		<script type="text/javascript" src="<?=$config['domain'] ?>/js/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="<?=$config['domain'] ?>/js/jquery-ui-1.10.4.custom.js"></script>
+		<script type="text/javascript" src="<?=$config['domain'] ?>/js/libs.js"></script>
+		<title><?=$config['title']?></title>
+	</head>
+	<body><br />
+		<center><h3>Покупка услуг</h3>
+		<form method="POST" action="" class="form-pay">
+			<div><input type="text" name="nikname" placeholder="Ник на сервере" class="form-control" /></div><br />
+			<?php if ( count($config['servers']) > 1 ) : ?>
+				<select name="servers" id="sel-servers" class="form-control">
+				<?php foreach ( $config['servers'] as $key => $val ) : ?>
+					<option value="<?=$key?>"><?=$val['name']?></option>
 				<?php endforeach; ?>
-			</select> <br>
-			<input type="submit" name="buy" value="Купить">
-		</form>
-	</div>
-	<br>
-	<div class="desc">
-		<h2 align="center">Описание товаров</h1>
-		<?php foreach($sdk->getProducts() as $product): ?>
-			<h4><?=$product['name']?></h4>
-			<p><?=$product['description']?></p>
-			<hr>
-		<?php endforeach; ?>
-	</div>
-	<br>
-	<div class="last">
-		<h2 align="center">Последние покупки</h2>
-		<?php foreach($sdk->getLastPurchases() as $purchase): ?>
-			<p>Игрок <b><?=$purchase['nickname']?></b></p>
-			<p>купил <?=is_array($purchase['product']) ? $purchase['product']['name'] : $purchase['product']?></p>
-			<p>за <?=($purchase['price'] + $purchase['commission'])?> руб.</p>
-			<hr>
-		<?php endforeach; ?>
-	</div>
-</body>
+				</select>
+				<br />
+				<?php foreach ( $config['servers'] as $key => $nope ) : ?>
+					<div id="view-server" class="class-<?=$key?>" style="display: none">
+						<select name="server" class="form-control">
+							<?php foreach ( $nope['groups'] as $val ) : ?>
+							<option value="<?=$val?>">[<?=$config['groups'][$val]['name']?>] <?=$config['groups'][$val]['price']?> Р. \ Сроком: <?php echo $config['groups'][$val]['time'] == 0 ? "Вечно" : $config['groups'][$val]['time'] ?></option>
+							<?php endforeach; ?>
+						</select>
+					</div>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<select name="group" class="form-control">
+					<?php foreach ( $config['groups'] as $val => $key ) : ?>
+					<option value="<?=$val?>">[<?=$key['name']?>] <?=$key['price']?> Р. \ Сроком: <?php echo $key['time'] == 0 ? "Вечно" : $key['time'] ?></option>
+					<?php endforeach; ?>
+				</select>
+			<?php endif; ?><br />
+			<div><input type="submit" class="btn btn-success" name="buy" value="Купить" /></div>
+		</form></center>
+		
+		<center style="margin-top: 100px;">
+			<?php foreach ( $monitoring as $key => $val ) : ?>
+				<?php if ( $val['status'] ) : ?>
+				<div style="width: 400px; text-align: center;">IP: <?php echo ! empty ( $val['server']['port'] ) ? $val['server']['host'].":".$val['server']['port'] : $val['server']['host'] ?></div>
+				<div style="width: 400px">Кол-во: <?=$val['numpl']?>, слотов: <?=$val['maxpl']?>, рекрод - <?=$val['record']?></div>
+				<div class="progress" style="width: 400px">
+					<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="<?=($val['numpl']/$val['maxpl'])*100?>" aria-valuemin="0" aria-valuemax="100" style="width: <?=($val['numpl']/$val['maxpl'])*100?>%">
+						<span class="sr-only"></span>
+					</div>
+				</div>
+				<?php else: ?>
+				<div style="width: 400px; text-align: center;">Сервер Offline</div>
+				<div class="progress" style="width: 400px">
+					<div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+						<span class="sr-only"></span>
+					</div>
+				</div>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		</center>
+	</body>
 </html>
